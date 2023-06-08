b1fe3b9833bff50d47d6ae3e687d92b2bedde6e6
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2023-06-09 07:23:18 +0800

    added destructuring

--- src/index.js
+++ src/index.js
@@ -2,50 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const firstBook = {
-  author: "Rick Rubin",
-  title: "The Creative Act",
-  img: "https://images-na.ssl-images-amazon.com/images/I/A1YhM3oRFbL._AC_UL600_SR600,400_.jpg",
-};
-const secondBook = {
-  author: "Dr. Seuss",
-  title: "Oh, the Places You'll Go!",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL600_SR600,400_.jpg",
-};
-const thirdBook = {
-  author: "Bonnie Garmus",
-  title: "Lessons in Chemistry",
-  img: "https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg",
-};
+const books = [
+  {
+    author: "Rick Rubin",
+    title: "The Creative Act",
+    img: "https://images-na.ssl-images-amazon.com/images/I/A1YhM3oRFbL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "Dr. Seuss",
+    title: "Oh, the Places You'll Go!",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    author: "Bonnie Garmus",
+    title: "Lessons in Chemistry",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-      <Book
-        author={thirdBook.author}
-        title={thirdBook.title}
-        img={thirdBook.img}
-      />
+      {books.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
     </section>
   );
 };
 const Book = (props) => {
+  const {
+    book: { author, title, img },
+  } = props;
   console.log(props);
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
